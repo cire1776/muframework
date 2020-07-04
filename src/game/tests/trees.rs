1bc7870bf9ad19b34396d43829b037e8e35634f6
Eric Russell <cire1776@gmail.com>
2020-07-04 17:07:33 -0400

    add tests for trees.

--- src/game/tests/trees.rs
+++ src/game/tests/trees.rs
@@ -620,6 +620,164 @@ fn player_earns_15_xp_harvesting_for_picking_an_olive() {
 }
 
 #[test]
+fn skilltime_is_accounted_for_while_picking_apples() {
+    let (
+        mut player,
+        mut map,
+        mut obstacles,
+        mut characters,
+        mut item_class_specifiers,
+        mut items,
+        mut facilities,
+        mut inventories,
+        mut _rng,
+        mut timer,
+        update_tx,
+        mut update_rx,
+        command_tx,
+        mut command_rx,
+        mut game_state,
+    ) = initialize_game_system_with_player_at(10, 9);
+
+    let mut rng = Rng::new();
+
+    player.endorse_with(":can_pick");
+    give_player_level(Harvesting, 12, &mut player);
+
+    // set facing to avoid change facing update
+    player.facing = Direction::Left;
+
+    let activity = game_state.game_loop_iteration(
+        &mut player,
+        &mut map,
+        &mut obstacles,
+        &mut characters,
+        &mut item_class_specifiers,
+        &mut items,
+        &mut facilities,
+        &mut inventories,
+        &mut rng,
+        &mut timer,
+        None,
+        &Command::Move(Direction::Left, MoveCommandMode::Use),
+        Some(&update_tx),
+        None,
+    );
+
+    assert_eq!(
+        timer.tags["ActivityComplete"],
+        TagType::Duration(chrono::Duration::seconds(49))
+    );
+
+    assert_activity_started(49_000, ui::pane::PaneTitle::PickingApples, &mut update_rx);
+
+    game_state.game_loop_iteration(
+        &mut player,
+        &mut map,
+        &mut obstacles,
+        &mut characters,
+        &mut item_class_specifiers,
+        &mut items,
+        &mut facilities,
+        &mut inventories,
+        &mut rng,
+        &mut timer,
+        activity,
+        &Command::ActivityComplete,
+        Some(&update_tx),
+        Some(command_tx),
+    );
+
+    assert_activity_expired(&mut update_rx);
+
+    assert_activity_started(49_000, ui::pane::PaneTitle::PickingApples, &mut update_rx);
+    assert_updates_are_empty(&mut update_rx);
+
+    assert_is_spawning_item(1, Food, "Apple", &mut command_rx);
+    assert_is_refresh_inventory(&mut command_rx);
+    assert_commands_are_empty(&mut command_rx);
+}
+
+#[test]
+fn skilltime_is_accounted_for_while_picking_olives() {
+    let (
+        mut player,
+        mut map,
+        mut obstacles,
+        mut characters,
+        mut item_class_specifiers,
+        mut items,
+        mut facilities,
+        mut inventories,
+        mut _rng,
+        mut timer,
+        update_tx,
+        mut update_rx,
+        command_tx,
+        mut command_rx,
+        mut game_state,
+    ) = initialize_game_system_with_player_at(10, 10);
+
+    let mut rng = Rng::new();
+
+    player.endorse_with(":can_pick");
+    give_player_level(Harvesting, 20, &mut player);
+
+    // set facing to avoid change facing update
+    player.facing = Direction::Left;
+
+    let activity = game_state.game_loop_iteration(
+        &mut player,
+        &mut map,
+        &mut obstacles,
+        &mut characters,
+        &mut item_class_specifiers,
+        &mut items,
+        &mut facilities,
+        &mut inventories,
+        &mut rng,
+        &mut timer,
+        None,
+        &Command::Move(Direction::Left, MoveCommandMode::Use),
+        Some(&update_tx),
+        None,
+    );
+
+    assert_eq!(
+        timer.tags["ActivityComplete"],
+        TagType::Duration(chrono::Duration::seconds(71))
+    );
+
+    assert_activity_started(71_000, ui::pane::PaneTitle::PickingOlives, &mut update_rx);
+
+    game_state.game_loop_iteration(
+        &mut player,
+        &mut map,
+        &mut obstacles,
+        &mut characters,
+        &mut item_class_specifiers,
+        &mut items,
+        &mut facilities,
+        &mut inventories,
+        &mut rng,
+        &mut timer,
+        activity,
+        &Command::ActivityComplete,
+        Some(&update_tx),
+        Some(command_tx),
+    );
+
+    assert_activity_expired(&mut update_rx);
+
+    assert_activity_started(71_000, ui::pane::PaneTitle::PickingOlives, &mut update_rx);
+    assert_updates_are_empty(&mut update_rx);
+
+    assert_is_spawning_item(1, Food, "Olive", &mut command_rx);
+    assert_is_refresh_inventory(&mut command_rx);
+    assert_commands_are_empty(&mut command_rx);
+}
+
+#[test]
 fn a_timer_is_set_for_fruit_and_wood_regeneration() {
     let (
         _player,
